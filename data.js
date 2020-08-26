c0ffeea41963fb08afc9168ec55155c21b92842c
e-bax <70255027+e-bax@users.noreply.github.com>
2020-08-26 16:16:49 +1000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,150 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano_20200728_115427_2",
+      "name": "PANO_20200728_115427_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.061167858169458356,
+        "pitch": 0.6945723339327223,
+        "fov": 1.3588273430044087
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6152455532147885,
+          "pitch": 0.13149134974593046,
+          "rotation": 0,
+          "target": "1-pano_20200728_115620_3"
+        },
+        {
+          "yaw": -2.779166572952679,
+          "pitch": 0.08959897864456323,
+          "rotation": 0,
+          "target": "2-pano_20200728_115200_0"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.1049174622994755,
+          "pitch": 0.06733509412945615,
+          "title": "Transfer of X - Damien Ietto",
+          "text": "Mildura Rural City Council transfer...x"
+        },
+        {
+          "yaw": 2.322568676611575,
+          "pitch": 0.17207288061171688,
+          "title": "Title",
+          "text": "www.adapt.com"
+        }
+      ]
+    },
+    {
+      "id": "1-pano_20200728_115620_3",
+      "name": "PANO_20200728_115620_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7005536216449073,
+          "pitch": 0.08573078260374345,
+          "rotation": 0,
+          "target": "0-pano_20200728_115427_2"
+        },
+        {
+          "yaw": -2.140829033109398,
+          "pitch": 0.2546493295402037,
+          "rotation": 0,
+          "target": "0-pano_20200728_115427_2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.8903250562971934,
+          "pitch": -0.1465142369847996,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-pano_20200728_115200_0",
+      "name": "PANO_20200728_115200_0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Loddon Mallee Renewable Energy Art Exhibition",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
